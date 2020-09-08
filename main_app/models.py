fddb63e832d31cdc8c947717ec63f6bc229ec6f5
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-09-08 14:20:45 -0400

    add class Album, class Collection, remove sublcass Collection and class albums

--- main_app/models.py
+++ main_app/models.py
@@ -6,9 +6,11 @@ from datetime import date
 # Import the User
 from django.contrib.auth.models import User
 
-class albums(models.Model):
-  name = models.CharField(max_length=50)
-  artist = models.CharField(max_length=20)
+class Album(models.Model):
+  name = models.CharField(max_length=100)
+  artist = models.CharField(max_length=100)
+  genre = models.CharField(max_length=100)
+  description = models.TextField
 
   def __str__(self):
     return self.name
@@ -16,15 +18,20 @@ class albums(models.Model):
   def get_absolute_url(self):
     return reverse('albums_detail', kwargs={'pk': self.id})
 
-    class Collection(models.Model):
-      name = models.CharField(max_length=100)
-      songs = models.CharField(max_length=100)
-      description = models.TextField(max_length=250)
-      albums = models.ManyToManyField(album)
-      user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # class Collection(models.Model):
+    #   name = models.CharField(max_length=100)
+    #   description = models.TextField(max_length=250)
+    #   albums = models.ManyToManyField(album)
+    #   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
 
-  def get_absolute_url(self):
-    return reverse('detail', kwargs={'collection_id': self.id})
+  # def get_absolute_url(self):
+  #   return reverse('detail', kwargs={'collection_id': self.id})
+
+class Collection(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  albums = models.ManyToManyField(Album)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
