5843776ac3baee444749a94f140a8642a1e9fbc4
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-09-08 09:12:01 -0400

    add album page path

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from. import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('album/<int:album_id>/', views.album_details, name='detail'),
   path('accounts/', include('django.contrib.auth.urls')),
   path('accounts/signup/', views.signup, name='signup'),
 ]
