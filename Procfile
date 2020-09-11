2da7f786944be99e4181bf63b01f66cdb33f890f
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-09-10 23:16:40 -0400

    remove database user info

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gaFm.wsgi
--- gaFm/settings.py
+++ gaFm/settings.py
@@ -83,8 +83,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'musiccontent',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'uyicool1$'
     }
 }
 
@@ -130,3 +128,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
+
+import django_heroku
+django_heroku.settings(locals())
